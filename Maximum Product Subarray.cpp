729dd784e7105bf5d7829e82093882222b11b680
Vaishnavi More <143414333+VaishnaviMore21@users.noreply.github.com>
2025-02-13 19:16:36 +0530

    Create Maximum Product Subarray.cpp

---
+++ Maximum Product Subarray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n=nums.size();
+        int pre=1,suff=1;
+        int ans=INT_MIN;
+        for(int i=0;i<n;i++)
+        {
+            if(pre==0)pre=1;
+            if(suff==0)suff=1;
+            pre*=nums[i];
+            suff*=nums[n-i-1];
+            ans=max(ans,max(pre,suff));
+        }
+    return ans;
+}
+};
