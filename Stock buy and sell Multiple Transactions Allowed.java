12f7aaa9de1c673d23a4df57ff9c78cf268a2850
Vaishnavi More <143414333+VaishnaviMore21@users.noreply.github.com>
2025-09-10 19:49:38 +0530

    Create Stock buy and sell Multiple Transactions Allowed.java

---
+++ Stock buy and sell Multiple Transactions Allowed.java
@@ -0,0 +1,11 @@
+class Solution {
+    int stockBuySell(int[] arr) {
+        int profit = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[i - 1]) {
+                profit += arr[i] - arr[i - 1];
+            }
+        }
+        return profit;
+    }
+}
